bdc9c07d3f12e45a93ea037ee327d1c5ea3ea2b6
nbheaven <noah.heaven77@gmail.com>
2024-09-21 21:51:01 -0600

    project finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"You are {first_name} {last_name}.");
     }
 }
